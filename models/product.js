5d66cb1f8a326e4c442726a030c6fb73db6050ae
Jeff Fredrickson <jeffrey.fredrickson@gsa.gov>
2017-10-25 14:28:47 -0400

    use sequelize v4 associate format

--- models/product.js
+++ models/product.js
@@ -1,15 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
-  var Product = sequelize.define('Product', {
+  const Product = sequelize.define('Product', {
     name: DataTypes.STRING,
     repository: DataTypes.STRING,
     description: DataTypes.STRING,
     laborHours: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
   })
+  Product.associate = (models) => {
+    // associations can be defined here
+  }
   return Product
 }
